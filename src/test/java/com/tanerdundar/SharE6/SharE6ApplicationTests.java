601cd72b9df9e649afb9f5263eeb67d04988240f
tanerdundar <t.dundar145@gmail.com>
2023-06-12 17:09:59 +0300

    set project

---
+++ src/test/java/com/tanerdundar/SharE6/SharE6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanerdundar.SharE6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharE6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
